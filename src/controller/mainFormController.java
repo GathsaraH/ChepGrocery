5865c66a42a2506ec5429406361d7f5b8e208e78
Gathsara <66924429+GathsaraH@users.noreply.github.com>
2021-10-09 10:54:03 +0530

    Create Customer section close #1

--- src/controller/mainFormController.java
+++ src/controller/mainFormController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+
 public class mainFormController {
+
+    public Button txtAddCustomer;
+    public Button txtAddItem;
+    public AnchorPane rootPain;
+
+    public void btnAddCustomer(MouseEvent mouseEvent) {
+
+    }
+
+    public void btnAddItem(MouseEvent mouseEvent) {
+    }
+
+    public void btnAddCustomer(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) this.rootPain.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("../view/CustomerForm.fxml"))));
+        stage.centerOnScreen();
+    }
+
+    public void btnAddItem(ActionEvent actionEvent) {
+    }
 }
